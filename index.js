2ac962671ef6b0c557b56eabc708ae0633a75a20
haysahakyan <sahakyanhayarpi0@gmail.com>
2022-06-02 23:24:56 -0700

    done_callback-practice

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+// const character = newImage('assets/green-character/static.gif')
+// move(character).withArrowKeys(100, 250)
 
 let direction = null;
 let x = 100;
@@ -48,9 +48,34 @@ document.addEventListener('keyup', function(e){
     direction = null
 })
 
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
-move(newImage('assets/tree.png')).to(200, 450)
+
+
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+// move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
